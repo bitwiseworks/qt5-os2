2d572feb6c9ede9f55d1e79926d5991a968f5e22
Dmitriy Kuminov <coding@dmik.org>
2019-08-29 23:22:37 +0400

    Enable initialization of Qt SVG module on OS/2.
    Needed for #4.

--- init-repository-os2.sh
+++ init-repository-os2.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# So far, we only support the qtbase submodule on OS/2.
-./init-repository --module-subset=qtbase "$@"
+# So far, we only support a subset of modules on OS/2.
+./init-repository --module-subset=qtbase,qtsvg --mirror=git://code.qt.io/qt/ "$@"
