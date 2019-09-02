b70205d8ee71bc1b6f3a9e683f74ccc0c6b9aaf8
Dmitriy Kuminov <coding@dmik.org>
2019-09-02 16:51:13 +0400

    Enable initialization of Qt Tools module on OS/2.
    This also switches to OS/2 repository and sticks with master branch.
    
    Needed for #6.

--- init-repository-os2.sh
+++ init-repository-os2.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # So far, we only support a subset of modules on OS/2.
-./init-repository --module-subset=qtbase,qtsvg --mirror=git://code.qt.io/qt/ "$@"
+./init-repository --module-subset=qtbase,qtsvg,qttools --mirror=git://code.qt.io/qt/ "$@"
